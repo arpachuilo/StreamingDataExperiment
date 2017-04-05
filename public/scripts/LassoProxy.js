6a66da972c873cfa1c261104afa8dd35ccb93b1c
arpachuilo <andrew.pachuilo@gmail.com>
2017-04-05 13:48:55 -0500

    Fixed clicking a lasso'd tweet

--- public/scripts/LassoProxy.js
+++ public/scripts/LassoProxy.js
@@ -21,6 +21,7 @@ function LassoProxy (g, margin, xScale, yScale, xValue, yValue, stream) {
     .subject(function() { var p = [d3.event.x, d3.event.y]; return [p, p]; })
     .on('start', dragstarted))
 
+  var draggin = false
   function dragstarted() {
     var d = d3.event.subject
     var active = proxyRegion.datum(d)
@@ -28,10 +29,12 @@ function LassoProxy (g, margin, xScale, yScale, xValue, yValue, stream) {
     var y0 = d3.event.y
 
     d3.event.on('start', function () {
+      dragging = false
       document.body.dispatchEvent(new Event('pause'))
     })
 
     d3.event.on('drag', function() {
+      dragging = true
       var x1 = d3.event.x
       var y1 = d3.event.y
       var dx = x1 - x0
@@ -43,22 +46,26 @@ function LassoProxy (g, margin, xScale, yScale, xValue, yValue, stream) {
     })
 
     d3.event.on('end', function () {
-      d3.selectAll(targetClass)
-        .filter(function (f) {
-          var pt = [xScale(+f[xValue]) + margin.left, yScale(+f[yValue]) + margin.top]
-          return d3.polygonContains(d, pt)
-        })
-        .each(function (d) {
-          var point = d3.select(this)
-          proxyTargets.append('path')
-            .datum(d)
-            .attr('class', 'proxy')
-            .attr('d', point.attr('d'))
-            .attr('transform', point.attr('transform'))
-            .on('click', point.on('click'))
-        })
+      if (dragging) {
+        dragging = false
+        proxyTargets.selectAll('*').remove()
+        d3.selectAll(targetClass)
+          .filter(function (f) {
+            var pt = [xScale(+f[xValue]) + margin.left, yScale(+f[yValue]) + margin.top]
+            return d3.polygonContains(d, pt)
+          })
+          .each(function (d) {
+            var point = d3.select(this)
+            proxyTargets.append('path')
+              .datum(d)
+              .attr('class', 'proxy')
+              .attr('d', point.attr('d'))
+              .attr('transform', point.attr('transform'))
+              .on('click', point.on('click'))
+          })
+        active.attr('d', null)
+      }
       document.body.dispatchEvent(new Event('resume'))
-      active.attr('d', null)
     })
   }
 
@@ -69,6 +76,7 @@ function LassoProxy (g, margin, xScale, yScale, xValue, yValue, stream) {
   })
 
   this.kill = function () {
+    g.on('.drag', null)
     proxyTargets.remove()
     proxyRegion.remove()
   }
