825021ab3adbbd6d31886fdf82cc69339b073c11
nhbain <bain607@gmail.com>
2017-04-21 13:31:43 -0500

    Fixed height for visualization to be 75% of users window.

--- public/scripts/BarAggregation.js
+++ public/scripts/BarAggregation.js
@@ -8,7 +8,7 @@ function BarAggregation(selection) {
     bottom: 45,
     left: 70
   }
-  var height = 420
+  var height = window.innerHeight * .75
   var width = selection.node().offsetWidth
   var chartWidth = width - margin.right - margin.left
   var chartHeight = height - margin.top - margin.bottom
