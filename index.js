322a512007caef761ca67d883cf8736c9a8d1886
arpachuilo <andrew.pachuilo@gmail.com>
2017-04-24 11:30:33 -0500

    now correctly storing data from multiple people

--- index.js
+++ index.js
@@ -27,80 +27,39 @@ if (CONFIG.LOGGING) {
   redisClient = redis.createClient()
 }
 
-var twitterClient
-if (CONFIG.LIVE_STREAM) {
-  twitterClient = new Twitter({
-    consumer_key: 'iCqI27Unu8I8z8X8ktqkqdekS',
-    consumer_secret: 'w7TqSQfdHQnGaF1NZEWZO18ahW7gFArIXfjKLTNhQ4lPxqf4m8',
-    access_token_key: '835131198108282880-RsY3sTnIiIUZMfSQfBap9BYFAqe5pLa',
-    access_token_secret: 'R3kPEA1lyFwdjzFw3RtoFuF0US9fBmpD3g2x1Sb9ztwRD'
-  })
-}
-
-var id, data, save
+var data, save
 
 if (CONFIG.LOGGING) {
   console.log('Setting up logger . . .')
-  id = ''
-  data = { id: id }
-  save = function () {
-    redisClient.set(id, JSON.stringify(data))
+  data = {}
+  save = function (id) {
+    redisClient.set(id, JSON.stringify(data[id]))
   }
-}
-
-var twitterStream
-if (CONFIG.LIVE_STREAM) {
-  twitterStream = twitterClient.stream('statuses/filter', CONFIG.STREAM_PARAMS)
-  console.log('Starting live twitter stream . . .')
 }
 
 io.on('connection', function (socket) {
 
   if (CONFIG.LOGGING) {
-    socket.on('id', function (value) {
-      id = value
-      data.id = id
-    })
+    data[socket.id] = {}
+    data[socket.id].id = socket.id
 
     socket.on('add', function (key, value) {
+      console.log(socket.id, data)
       if (key in data) {
-        data[key].push(value)
+        data[socket.id][key].push(value)
       } else {
-        data[key] = []
-        data[key].push(value)
+        data[socket.id][key] = []
+        data[socket.id][key].push(value)
       }
-      save()
+      save(socket.id)
     })
 
     socket.on('set', function (key, value) {
-      data[key] = value
+      data[socket.id][key] = value
     })
 
     socket.on('save', function () {
       save()
-    })
-  }
-
-  if (CONFIG.LIVE_STREAM) {
-    twitterStream.on('data', function (tweet) {
-      try {
-        autolinker.parse(tweet, function(err, result) {
-          var text = result.html
-          var data = {
-            id: tweet.id,
-            rt_overtime: tweet.retweet_count,
-            fav_overtime: tweet.favorite_count,
-            time: tweet.timestamp_ms,
-            user: tweet.user.screen_name,
-            profile_pic: tweet.user.profile_image_url,
-            followers: tweet.user.followers_count,
-            following: tweet.user.friends_count,
-            text: tweet.text,
-            fixed_text: text
-          }
-          socket.emit('tweet', JSON.stringify(data))
-        })
-      } catch (e) {}
     })
   }
 })
